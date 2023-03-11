961cc159230a24597e4c16ca7417348b70c5c04d
kaido12 <kaidofalcon@gmail.com>
2023-03-11 20:40:52 +0530

    11 march  9pm

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 
 const BreadCrumb = ({title}) => {
   return (
-    <div className='breadcrumb mb-0 py-5 px-5'>
+    <div className='breadcrumb mb-0 py-3 px-5'>
         <div className="container-xxl">
             <div className="row">
                 <div className="col-12">
-                    <p className='text-start mb-0'>
+                    <p className='text-center mb-0'>
                         <Link to="/" className='text-dark'>Home </Link>
                         {" "} / {title}
                     </p>
